b0f8a8b58b9f1713dd56443807f8432964d0130c
EdynsonRonny <kratos_pxndx@hotmail.com>
2019-01-11 13:29:01 -0500

    11-01-19 MODIFICADO

--- ViceAdmin/config/conexion_sis.php
+++ ViceAdmin/config/conexion_sis.php
@@ -1,6 +1,6 @@
 <?php
 	$serverName = "localhost";
-	$connectionInfo = array("Database"=>"DB_VicerrectoradoInv", "UID"=>"sa", "PWD"=>"1234", "CharacterSet"=>"UTF-8");
+	$connectionInfo = array("Database"=>"DB_VicerrectoradoInv", "UID"=>"sa", "PWD"=>"123", "CharacterSet"=>"UTF-8");
 	$con = sqlsrv_connect($serverName, $connectionInfo);
 
 	if($con){
